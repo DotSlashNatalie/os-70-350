4c48905601371a0463e522cd701c1ce2c1846274
Nathan Adams <adamsna@datanethost.net>
2013-12-03 21:37:10 -0600

    updating shell script

---
+++ ext/download_tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+packages="install qemu qemu-system nasm"
+
+distro=`lsb_release -a 2> /dev/null | head -1 | cut -f 2 -d':' | tr -d "\t"`
+if [ "$disto" == "Debian" -o "$disto" == "Ubuntu" ]
+then
+	apt-get $packages
+else
+	yum install $packages
+fi
+
