0e9a3b335d8a43baad3cfe75e113648867cc0edd
Nathan Adams <adamsna@datanethost.net>
2013-12-03 20:12:03 -0600

    Adding download script for windows

---
+++ ext/download_tools.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if NOT [%1] == [] goto end2
+
+:start
+bitsadmin /create test > log.txt
+bitsadmin /addfile test http://cdn.srchub.org/ostools.exe %CD%\ostools.exe >> log.txt
+bitsadmin /resume test >> log.txt
+bitsadmin /setnotifycmdline test "%SystemRoot%\system32\cmd.exe" "/c %CD%\download_tools.bat %CD%" >> log.txt
+::bitsadmin /setnotifycmdline test "%SystemRoot%\system32\bitsadmin.exe" "%SystemRoot%\system32\bitsadmin.exe /complete test"
+goto end3
+
+:end2
+bitsadmin.exe /complete test > nul
+%1\ostools.exe -gm2 -y
+
+:end3
